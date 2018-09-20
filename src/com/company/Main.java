0e3ce65ba2e0a8e2ca90cfafb1a9123e00544581
Steffen Rasmussen <42993915+TheWildDane@users.noreply.github.com>
2018-09-20 13:59:55 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+
+import java.io.Console;
+import java.util.Random;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+
+        Random Randomizer = new Random();
+        Console input = new Console();
+        int choice;
+        String selection;
+        String[][] QuestionList = new String[4][2];
+        QuestionList[3][0] = "My Question \nA: Answer One\nB: Answer Two\nC: Answer Three";
+        QuestionList[3][1] = "Correct Answer";
+
+    }
+
+}
